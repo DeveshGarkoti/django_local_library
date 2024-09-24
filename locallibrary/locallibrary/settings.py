5744fe1f89861488797e4e2a97b77e5f2777fb16
DeveshGarkoti <deveshgar280@gmail.com>
2024-09-24 12:47:53 -0700

    view

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Add our new application
-    'catalog.apps.CatalogConfig', # This object was created for us in /catalog/apps.py
+    'catalog', # This object was created for us in /catalog/apps.py
 ]
 
 MIDDLEWARE = [
