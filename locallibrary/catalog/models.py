b23b26d1559f089fe44d0c6d18c6c2836ba61e6f
DeveshGarkoti <deveshgar280@gmail.com>
2024-09-26 17:50:07 -0700

    forms

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -4,6 +4,10 @@ from django.db import models
 from django.urls import reverse # Used in get_absolute_url() to get URL for specified ID
 from django.db.models import UniqueConstraint # Constrains fields to unique values
 from django.db.models.functions import Lower # Returns lower cased value of field
+from django.conf import settings
+from django.contrib.auth.models import User
+from datetime import date
+
 
 import uuid # Required for unique book instances
 
@@ -75,6 +79,8 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -94,9 +100,16 @@ class BookInstance(models.Model):
     class Meta:
         ordering = ['due_back']
 
+        permissions = (("can_mark_returned", "Set book as returned"),)
+
     def __str__(self):
         """String for representing the model object."""
         return f'{self.id} ({self.book.title})'
+    
+    @property
+    def is_overdue(self):
+        """Determines if the book id overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
     
 
 class Author(models.Model):
@@ -146,4 +159,3 @@ class Language(models.Model):
     def get_absolute_url(self):
         """Returns the URL to access a detail record for this book."""
         return reverse('language-detail', args=[str(self.id)])
-
